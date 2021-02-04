b5b993137e10b12c4ccc64ae05a99b36f4e714f7
Heri Susanto <herisusanto.id@gmail.com>
2021-02-04 16:55:17 +0700

    Initial Commit

---
+++ StormViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StormViewer
+//
+//  Created by Heri Susanto on 04/02/21.
+//  Copyright © 2021 Heri Susanto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
